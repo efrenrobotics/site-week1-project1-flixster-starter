4a328efb363ab6bb15492cef7a4591d9fce22314
Efren Mendoza Enriquez <efren.mendoza@efrenme-ltmnuyv.internal.salesforce.com>
2023-06-08 23:07:38 -0700

    add-movies

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const grid = document.querySelector("#movies-grid");
-const movieBtn = document.querySelector("#movie-btn");
-
-const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=e6df5ce0d6c30a501b6e37014f9b5766&page=1';
-const apiBaseURL = 'https://api.themoviedb.org/3';
 const apiKey = 'e6df5ce0d6c30a501b6e37014f9b5766';
+let pageNum = 1;
 
 /***** Movie Card *****/
 // @movieInfo - would be the array of JSON movies
@@ -16,7 +13,6 @@ function loadCards(movieInfo) {
         ********* title ***********
         ********* rating **********
         */
-
         const movieCard = document.createElement("div");
         movieCard.setAttribute("class", "movie-card");
 
@@ -32,7 +28,7 @@ function loadCards(movieInfo) {
 
         const rating = document.createElement('h3');
         rating.setAttribute("class", "movies-votes")
-        rating.innerText = 'Rating: ${movie.vote_average}';
+        rating.innerText = `Rating: ${movie.vote_average}`;
 
         movieCard.appendChild(image);
         movieCard.appendChild(title);
@@ -42,26 +38,25 @@ function loadCards(movieInfo) {
     });
 }
 
-movieBtn.addEventListener("click", function () {
-    createMovieCard();
-});
-
-/* LOAD MORE - load N movies into the screen */
+/* LOAD MORE - load next movie page into the screen */
 const loadBtn = document.querySelector("#load-movies");
 loadBtn.addEventListener("click", function () {
-    for(let n = 0; n < 5; n++)
-        createMovieCard();
+    apiCall();
 });
 
+
 /***** FETCH API *****/ 
+/* Async functions, serve as futures and waits for data to fetch and process to finish */
 const apiCall = async() => {
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${pageNum}`;
     const res = await fetch(url);
     const data = await res.json();
     const results = data.results;
-    console.log(results);
-
     loadCards(results);
+    pageNum++;
 }
+
+/* Initial movie load */
 apiCall();
 
 
